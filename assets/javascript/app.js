91b1daf9a47ff88bfe3134506af4b318828c096a
CS <Cara@MBP15in2015.local>
2019-06-25 22:29:57 -0400

    Wrote HTML/Bootstrap basics and initalized Firebase

---
+++ assets/javascript/app.js
@@ -0,0 +1,19 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBIdaZ4PboKCjP8iOZnZm96MRDBHEzKKgs",
+    authDomain: "train-scheduler-48407.firebaseapp.com",
+    databaseURL: "https://train-scheduler-48407.firebaseio.com",
+    projectId: "train-scheduler-48407",
+    storageBucket: "",
+    messagingSenderId: "892783396057",
+    appId: "1:892783396057:web:4f77d11b901d0b46"
+  };
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Creating database variable
+var database = firebase.database();
+
+
+
+// https://console.firebase.google.com/u/0/project/train-scheduler-48407/overview
