de7f28b40dfcb8bfdd92e270a38c371bb13f8de4
CS <Cara@MBP15in2015.local>
2019-06-25 22:46:53 -0400

    JS written to push user input to Firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,6 +14,46 @@ firebase.initializeApp(firebaseConfig);
 // Creating database variable
 var database = firebase.database();
 
+// Submit button for adding a new train
+$("#submit").on("click", function(event){
+    event.preventDefault();
+    // Grab user input
+    var trainName = $("#train-name").val().trim();
+    var destination = $("#destination").val().trim();
+    var firstTrainTime = $("#first-train").val().trim();
+    var frequency = $("#frequency").val().trim();
 
+    // Creates local "temporary" object for holding employee data
+    var temp = {
+        train: trainName,
+        destination: destination,
+        firstTrain: firstTrainTime,
+        frequency: frequency
+    }
+
+    // *** Does it matter whether I create a temporary object and then push that object to Firebase or push all of the items at once?
+
+    // Upload train data to the database
+    // database.ref().push({
+    //     train: trainName,
+    //     destination: destination,
+    //     firstTrain: firstTrainTime,
+    //     frequency: frequency
+    // });
+    database.ref().push(temp);
+
+    // Log user input to the console
+    console.log("Train name: " + trainName);
+    console.log("Destination: " + destination);
+    console.log("First train time: " + firstTrainTime);
+    console.log("Frequency: " + frequency);
+
+    // Clear all of the text boxes
+    $("#train-name").val("");
+    $("#destination").val("");
+    $("#first-train").val("");
+    $("#frequency").val("");
+
+});
 
 // https://console.firebase.google.com/u/0/project/train-scheduler-48407/overview
