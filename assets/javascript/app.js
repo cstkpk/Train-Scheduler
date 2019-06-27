81060425b1922fc30ed06d7776bb1a8a0b79fcb2
CS <Cara@MBP15in2015.local>
2019-06-27 17:15:27 -0400

    Next train time and minutes away now update every 30 seconds

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,12 +17,19 @@ var database = firebase.database();
 // Submit button for adding a new train
 $("#submit").on("click", function(event){
     event.preventDefault();
+
     // Grab user input
     var trainName = $("#train-name").val().trim();
     var destination = $("#destination").val().trim();
-    var firstTrainTime = moment($("#first-train").val().trim(), "HH:mm").format("X");
-    // console.log(firstTrainTime);
+    // var firstTrainTime = moment($("#first-train").val().trim(), "HH:mm").format("X");
+    var firstTrainTime = $("#first-train").val().trim();
     var frequency = $("#frequency").val().trim();
+    
+    if (!trainName || !destination || !firstTrainTime || !frequency) {
+        // alert("Please fill out the form");
+        $("#modal-test").modal("show");
+        return false;
+    }
 
     // Creates local "temporary" object for holding train data
     var temp = {
@@ -32,22 +39,14 @@ $("#submit").on("click", function(event){
         frequency: frequency
     }
 
-    // *** Does it matter whether I create a temporary object and then push that object to Firebase or push all of the items at once?
-
     // Upload train data to the database
-    // database.ref().push({
-    //     train: trainName,
-    //     destination: destination,
-    //     firstTrain: firstTrainTime,
-    //     frequency: frequency
-    // });
     database.ref().push(temp);
 
     // Log user input to the console
-    console.log("Train name: " + trainName);
-    console.log("Destination: " + destination);
-    console.log("First train time: " + firstTrainTime);
-    console.log("Frequency: " + frequency);
+    // console.log("Train name: " + trainName);
+    // console.log("Destination: " + destination);
+    // console.log("First train time: " + firstTrainTime);
+    // console.log("Frequency: " + frequency);
 
     // Clear all of the text boxes
     $("#train-name").val("");
@@ -58,44 +57,85 @@ $("#submit").on("click", function(event){
 });
 
 // Create Firebase event for adding train to the database and a row in the html when a user adds an entry
-database.ref().on('child_added', function(childSnapshot) {
-    console.log(childSnapshot.val());
+// database.ref().on('child_added', function(childSnapshot) {
+//     console.log(childSnapshot.val());
 
-    // Store everything into a variable.
-    var trainName = childSnapshot.val().train;
-    var destination = childSnapshot.val().destination;
-    var firstTrainTime = childSnapshot.val().firstTrain;
-    var frequency = childSnapshot.val().frequency;
-    console.log(firstTrainTime);
-    console.log(typeof(firstTrainTime));
+//     // Store everything into a variable.
+//     var trainName = childSnapshot.val().train;
+//     var destination = childSnapshot.val().destination;
+//     var firstTrainTime = childSnapshot.val().firstTrain;
+//     var frequency = childSnapshot.val().frequency;
+//     // console.log("First train time: " + moment(firstTrainTime, "X").format("HH:mm"));
+//     console.log("First train time: " + firstTrainTime);
 
-    // Calculate minutes away by subtracting firstTrainTime from current time and finding the modulus between this difference and frequency
-    var remainder = moment().diff(moment.unix(parseInt(firstTrainTime)), "minutes") % frequency;
-    console.log("Remainder: " + remainder);
-    // Then find difference between frequency and remainder
-    var minAway = frequency - remainder;
-    console.log(minAway);
+//     // Calculate minutes away by first:
+//     // Subtracting firstTrainTime from current time and finding the modulus between this difference and frequency
+//     // var remainder = moment().diff(moment.unix(parseInt(firstTrainTime)), "minutes") % frequency;
+//     var remainder = moment().diff(moment(firstTrainTime, "HH:mm"), "minutes") % frequency;
+//     // Then find difference between frequency and remainder
+//     var minAway = frequency - remainder;
+//     console.log("Minutes away: " + minAway);
 
-    // Calculate next arrival
-    var nextArrival = moment().add((minAway), "m").format("HH:mm");
-    console.log(nextArrival);
+//     // Calculate next arrival
+//     var nextArrival = moment().add((minAway), "m").format("hh:mm a");
+//     console.log("Next arrival: " + nextArrival);
 
-
-
-
-
-    // Create the new row
-    var newRow = $("<tr>").append(
-        $("<td>").text(trainName),
-        $("<td>").text(destination),
-        $("<td>").text(frequency),
-        $("<td>").text(nextArrival),
-        $("<td>").text(minAway)
-    );
+//     // Create the new row
+//     var newRow = $("<tr>").append(
+//         $("<td>").text(trainName),
+//         $("<td>").text(destination),
+//         $("<td>").text(frequency),
+//         $("<td>").text(nextArrival),
+//         $("<td>").text(minAway)
+//     );
     
-    // Append the new row to the table
-    $("#train-schedule").append(newRow);
+//     // Append the new row to the table
+//     $("#train-schedule").append(newRow);
 
-});
+// });
+
+function trainTimes() {
+    $("tbody").empty();
+    database.ref().on('child_added', function(childSnapshot) {
+        console.log(childSnapshot.val());
+    
+        // Store everything into a variable.
+        var trainName = childSnapshot.val().train;
+        var destination = childSnapshot.val().destination;
+        var firstTrainTime = childSnapshot.val().firstTrain;
+        var frequency = childSnapshot.val().frequency;
+        // console.log("First train time: " + moment(firstTrainTime, "X").format("HH:mm"));
+        console.log("First train time: " + firstTrainTime);
+    
+        // Calculate minutes away by first:
+        // Subtracting firstTrainTime from current time and finding the modulus between this difference and frequency
+        // var remainder = moment().diff(moment.unix(parseInt(firstTrainTime)), "minutes") % frequency;
+        var remainder = moment().diff(moment(firstTrainTime, "HH:mm"), "minutes") % frequency;
+        // Then find difference between frequency and remainder
+        var minAway = frequency - remainder;
+        console.log("Minutes away: " + minAway);
+    
+        // Calculate next arrival
+        var nextArrival = moment().add((minAway), "m").format("hh:mm a");
+        console.log("Next arrival: " + nextArrival);
+    
+        // Create the new row
+        var newRow = $("<tr>").append(
+            $("<td>").text(trainName),
+            $("<td>").text(destination),
+            $("<td>").text(frequency),
+            $("<td>").text(nextArrival),
+            $("<td>").text(minAway)
+        );
+        
+        // Append the new row to the table
+        $("#train-schedule").append(newRow);
+    
+    });
+    
+}
+trainTimes();
+setInterval(trainTimes, 20000)
+
 
 // https://console.firebase.google.com/u/0/project/train-scheduler-48407/overview
